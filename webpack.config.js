d143996f2d37fb7818ddecd1808cef069fdfa654
wvha <Wvha17@gmail.com>
2018-05-03 21:04:11 -0700

    initial COMMIT

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+var webpack = require("webpack");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.jsx',
+    path: DIST_DIR
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+       }
+      }
+    ]
+  }
+};
