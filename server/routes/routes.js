50917083146e3fa0219197f8f30d803f854b5421
wvha <Wvha17@gmail.com>
2018-05-05 10:13:49 -0700

    commit before pulling down postgres file setup

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const controller = require('../controller.js');
+// const controller = require('../controller.js');
 
 // router.post('/api/signup', controller.post.signup);
 
