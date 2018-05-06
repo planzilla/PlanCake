83b2391273711e3c6d8d8eef214ceb606f30e18b
brandon-villiados <brandon.villiados@gmail.com>
2018-05-05 22:52:40 -0700

    Splash/Dashboard Styling --still needs routes set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,46 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
+import axios from 'axios';
 import NavBar from './components/header/NavBar.jsx';
 import SplashPage from './components/body/SplashPage.jsx';
 import ContactInfo from './components/footer/ContactInfo.jsx';
 import Dashboard from './components/body/Dashboard.jsx';
 // import Test from './test.jsx' (used to test rendering of components)
-=======
-import { Route, Switch } from 'react-router-dom';
->>>>>>> bcccd3fd9f8a5d7dd17d29f510fff6416fb4b1ff
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    axios.post('/api/user', {firstName: "Brandon", lastName: "Villiados"})
+    .then((data) => {console.log("post was a success", data)})
+  }
+
   render() {
     return (
-<<<<<<< HEAD
-      // <div className="splash grid">
-      //   <NavBar />
-      //   <SplashPage />
-      //   <ContactInfo />
-      // </div>
-
-      <div className="dashboard grid">
-      <NavBar />
-      <Dashboard />
-      <ContactInfo />
-=======
-      <div style={{backgroundColor: "#fffff"}}>
-
-        <Switch>
-          <Route exact path="/" render={ props => <h1>hello react</h1> } />
-          {/* <Route path="/example" component={ Example } /> */}
-          {/* <Route path="/example2" render={ props => {
-            return (
-              <Example2 function={ function }/>
-            )}}
-          /> */}
-
-          </Switch>
->>>>>>> bcccd3fd9f8a5d7dd17d29f510fff6416fb4b1ff
+      <div className="splash grid">
+        <NavBar />
+        <SplashPage />
+        <ContactInfo />
       </div>
+      // == route to splash if not logged in ==
+
+      // <div className="full-height-width grid">
+      // <NavBar/>
+      // <Dashboard />
+      // <ContactInfo />
+      // == route after login is authenitcated == 
+
+        // <Switch>
+        //   <Route exact path="/" render={ props => <h1>hello react</h1> } />
+        //   <Route path="/example" component={ Example } />
+        //   <Route path="/example2" render={ props => {
+        //     return (
+        //       <Example2 function={ function }/>
+        //     )}}
+        //   />
+
+        //   </Switch> 
     )
   }
 };
--- client/src/components/body/Dashboard.jsx
+++ client/src/components/body/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+//import sidebar
+//import event cards
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -7,8 +9,9 @@ export default class Dashboard extends Component {
 
   render() {
     return(
-      <div >
-        <h1>Dashboard body</h1>
+      <div className="dashboard grid">
+        <div className="sidebar">sidebar</div>
+        <div className="event-cards">eventcards</div>
       </div>
     )
   }
