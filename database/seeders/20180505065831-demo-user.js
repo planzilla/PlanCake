53bb065b1a9f9ec0b435442d2249a717dcd0b8c3
Christina <christina.elaine.yuen@gmail.com>
2018-05-08 14:36:01 -0700

    Updated database files so that date is created automatically for createdAt and updatedAt

--- database/seeders/20180505065831-demo-user.js
+++ database/seeders/20180505065831-demo-user.js
@@ -9,24 +9,21 @@ module.exports = {
           lastName: 'Yuen',
           email: 'cey@cey.com',
           username: 'cey',
-          password: 'cey',
-          createdAt: '2018-05-05T06:52:26.282Z'
+          password: 'cey'
         },
         {
           firstName: 'Will',
           lastName: 'Ha',
           email: 'wvha@wvha.com',
           username: 'wvha',
-          password: 'wvha',
-          createdAt: '2018-05-06T06:52:26.282Z'
+          password: 'wvha'
         },
         {
           firstName: 'Brandon',
           lastName: 'Villiados',
           email: 'bv@bv.com',
           username: 'bv',
-          password: 'bv',
-          createdAt: '2018-05-07T06:52:26.282Z'
+          password: 'bv'
         },
       ], {});
   },
