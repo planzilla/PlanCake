875c174cf951abec0e100c5ec9690fb4c8da8e3f
Christina <christina.elaine.yuen@gmail.com>
2018-05-09 00:59:52 -0700

    Made createEvent into modal. button appears on navbar temporarily

--- client/src/components/body/CreateEvent.jsx
+++ client/src/components/body/CreateEvent.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import { Button, Header, Icon, Image, Modal, Form, Input, TextArea } from 'semantic-ui-react';
 
-const FormExampleFieldControlId = () => (
-  <Form>
-    <Form.Group widths='equal'>
-      <Form.Field
-        id='form-input-control-title'
-        control={Input}
-        label='Title'
-        placeholder='Title'
-      />
-      <Form.Field
-        id='form-input-control-location' 
-        control={Input}
-        label='Location'
-        placeholder='Location'
-      />
-    </Form.Group>
-    <Form.Input
-      id='form-textarea-control-invite'
-      control={TextArea}
-      label='Invite Friends'
-      placeholder='Enter email addresses'
-    />
-    <Form.Field
-      id='form-button-control-public'
-      control={Button}
-      content='Confirm'
-      label='Create Event'
-    />
-  </Form>
+const CreateEvent = () => (
+  <Modal trigger={<Button>Create an Event</Button>} closeIcon>
+    <Modal.Header>Create an Event</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field
+            id='form-input-control-title'
+            control={Input}
+            label='Title'
+            placeholder='Title'
+          />
+          <Form.Field
+            id='form-input-control-location'
+            control={Input}
+            label='Location'
+            placeholder='Location'
+          />
+        </Form.Group>
+        <Form.Input
+          id='form-textarea-control-invite'
+          control={TextArea}
+          label='Invite Friends'
+          placeholder='Enter email addresses'
+        />
+      </Form>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button basic color='red'>
+        <Icon name='remove' /> Cancel
+      </Button>
+      <Button color='green'>
+        <Icon name='checkmark' /> Create
+      </Button>
+    </Modal.Actions>
+  </Modal>
 );
 
-export default FormExampleFieldControlId;
+export default CreateEvent;
--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -3,6 +3,7 @@ import Login from './Login.jsx';
 import Signup from './SignUp.jsx';
 import Modal from 'react-modal';
 import axios from 'axios';
+import CreateEvent from '../body/CreateEvent.jsx';
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -39,6 +40,7 @@ export default class NavBar extends Component {
         <div>
           <img src="plancake2.png" alt="plancake.png"/>
         </div>
+        <CreateEvent />
         <div className="login jsas">
           <a onClick={this.handleModal.bind(this)}>Login</a>
           <Modal
