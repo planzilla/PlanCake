31a9a906e7b8f40be1c92aa9d29d91d5ee4de04d
Christina <christina.elaine.yuen@gmail.com>
2018-05-10 21:20:57 -0700

    Working sidebar

--- client/src/components/body/SideBar.jsx
+++ client/src/components/body/SideBar.jsx
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import { Accordion, Icon } from 'semantic-ui-react'
 import CreateEvent from './CreateEvent.jsx';
 
 export default class SideBar extends Component {
   constructor(props) {
     super(props);
+    this.state = { activeIndex: 0 };
+    this.handleClick = this.handleClick.bind(this);
   }
-  
+
+  handleClick(e, titleProps) {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
   render() {
-    return(
+    const { activeIndex } = this.state
+    return (
       <div className="sidebar">
         <CreateEvent
           handleCreateEvent={this.props.handleCreateEvent}
           handleInputChange={this.props.handleInputChange}
           createEventError={this.props.createEventError}
-        />        
+        />
+        <Accordion>
+          {this.props.events
+            ? this.props.events.map((event, i) => {
+              return (<div key={i}>
+                <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
+                  <Icon name='dropdown' />
+                  <b>{event.title}</b>
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === i}>
+                  <p>
+                    Create a topic board
+                </p>
+                </Accordion.Content>
+              </div>)
+            })
+            : null}
+        </Accordion>
       </div>
     )
   }
