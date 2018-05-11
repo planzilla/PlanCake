401937ef497261a4e0acb963c4c9bd86a2e7ea1c
Christina <christina.elaine.yuen@gmail.com>
2018-05-11 10:50:29 -0700

    one sucessful test!

--- __tests__/__tests___database/createEvent.test.js
+++ __tests__/__tests___database/createEvent.test.js
@@ -1,10 +1,27 @@
-import { get, post, patch } from '../server/controller';
-import { CreateEvent } from '../client/src/components/body';
+import { get, post, patch } from '../../server/controller.js';
+import { Event } from '../../database/models';
 
+const clearDatabase = () => {
+  User.destroy({
+    where: {},
+    truncate: true
+  })
+}
 
 test('Should add event to Event model', () => {
-  const title = 'Testing title';
-  const location = 'Seattle, WA';
-
+  const titleTest = 'Testing title';
+  const locationTest = 'Seattle, WA';
   
+  post.createEvent({
+    body: {
+      createEventTitle: titleTest,
+      createEventLocation: locationTest
+    }
+  })
+  .then(() => Event.findOne({
+    where: {titleTest}
+  }))
+  .then((event) => expect(event.location).toBe('Seattle, WA'))
 })
+
+afterAll(clearDatabase);
