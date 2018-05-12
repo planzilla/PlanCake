c5b27b989446011a531fb0bb85b645d0663281f6
Christina <christina.elaine.yuen@gmail.com>
2018-05-11 17:04:22 -0700

    MOdified eventcard to be stateless

--- client/src/components/body/Dashboard.jsx
+++ client/src/components/body/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import EventCard from './EventCard.jsx';
-import { Card } from 'semantic-ui-react'
 
 //import sidebar
 //import event cards
@@ -18,14 +17,11 @@ export default class Dashboard extends Component {
     return(
       <div className="dashboard grid">
         <div className="event-cards">
-        {console.log(this.props.events)}
-        <Card.Group>
         {this.props.events.map((event, i) => {
           return(
               <EventCard title={event.title} location={event.location} key={i}/>
           )
         })}
-        </Card.Group>
         </div>
       </div>
     )
--- client/src/components/body/EventCard.jsx
+++ client/src/components/body/EventCard.jsx
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import { Card, Icon } from 'semantic-ui-react'
 
-export default class EventCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
+const EventCard = ({ title, location }) => (
+  <Card fluid color='teal'>
+    <Card.Content header={title} />
+    <Card.Content>
+      <p><b>ToDo</b></p>
+      <ul>
+        <li> Book Flights </li>
+        <li> Contact Airbnb Host </li>
+      </ul>
+    </Card.Content>
+    <Card.Content extra>
+      <Icon name='map pin' />
+      {location}
+    </Card.Content>
+  </Card>
+)
 
-  render() {
-    return (
-      <Card fluid color='teal'>
-        <Card.Content header={this.props.title} />
-        <Card.Content>
-          <p><b>ToDo</b></p>
-          <ul>
-            <li> Book Flights </li>
-            <li> Contact Airbnb Host </li>
-          </ul>
-        </Card.Content>
-        <Card.Content extra>
-          <Icon name='map pin' />
-          {this.props.location}
-        </Card.Content>
-      </Card>
-    )
-  }
-}
+export default EventCard;
