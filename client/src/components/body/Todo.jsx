f3920cacad739ed49a5d761af3b5f659715cee7e
wvha <Wvha17@gmail.com>
2018-05-17 10:23:44 -0700

    todos rendering correctly with checkbox

--- client/src/components/body/Todo.jsx
+++ client/src/components/body/Todo.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'semantic-ui-react';
+import moment from 'moment';
 
 export default class Todo extends Component {
   constructor(props) {
@@ -12,11 +14,13 @@ export default class Todo extends Component {
         <h3>Your To-Do List</h3>
           {this.props.todos.map(todo => {
             if (todo.EventId === this.props.event.id) {
+              // let date = moment(todo.deadline, "DD MMMM YY");
               console.log(todo);
               return (
                 <div>
-                  <p>{todo.text}</p>
-                  <p>{todo.deadline}</p>
+                  <p><Checkbox label={todo.text} /> 
+                  due: {moment(todo.deadline).format("DD MMMM YY")}
+                  </p>
                 </div>
               )
             }
